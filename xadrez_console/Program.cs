5bb25b99538de59adffbf4b80228c3b339351424
João Victor <joaovictor.sgoncalves@hotmail.com>
2020-04-20 17:21:33 -0300

    Implementando o metodo tela que imprime o tabuleiro

--- xadrez_console/Program.cs
+++ xadrez_console/Program.cs
@@ -7,10 +7,11 @@ namespace xadrez_console
     {
         static void Main(string[] args)
         {
-            Posicao p = new Posicao(3, 4);
+            
             Tabuleiro tab = new Tabuleiro(8, 8);
 
-            Console.WriteLine("Posição: " + p);
+
+            Tela.ImprimirTabuleiro(tab);
         }
     }
 }
--- xadrez_console/Tabuleiro/Tabuleiro.cs
+++ xadrez_console/Tabuleiro/Tabuleiro.cs
@@ -17,5 +17,10 @@ namespace tabuleiro
             Pecas = new Peca[Linhas, Colunas];
 
         }
+
+        public Peca Peca(int linha , int coluna)
+        {
+            return Pecas[linha, coluna];
+        }
     }
 }
